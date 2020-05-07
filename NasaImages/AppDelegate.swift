51fb2c4388d2d30079eef758ad60c7fc4d5d1ef9
Robert Dresler <robertdreslerjr@gmail.com>
2020-05-07 09:25:01 +0200

    Changed: Renaming

--- NasaImages/AppDelegate.swift
+++ NasaImages/AppDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.swift
-//  NasaImages
+//  NASAImages
 //
 //  Created by Robert Dresler on 07/05/2020.
 //  Copyright © 2020 Robert Dresler. All rights reserved.
--- NasaImages/ViewController.swift
+++ NasaImages/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  NasaImages
+//  NASAImages
 //
 //  Created by Robert Dresler on 07/05/2020.
 //  Copyright © 2020 Robert Dresler. All rights reserved.
