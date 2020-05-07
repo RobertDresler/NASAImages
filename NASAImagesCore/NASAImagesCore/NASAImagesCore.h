7ca50047a696f7808a2b3449c164eba0b8becfbd
Robert Dresler <robertdreslerjr@gmail.com>
2020-05-07 09:44:33 +0200

    Changed: Setup modules

---
+++ NASAImagesCore/NASAImagesCore/NASAImagesCore.h
@@ -0,0 +1,19 @@
+//
+//  NASAImagesCore.h
+//  NASAImagesCore
+//
+//  Created by Robert Dresler on 07/05/2020.
+//  Copyright © 2020 Robert Dresler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NASAImagesCore.
+FOUNDATION_EXPORT double NASAImagesCoreVersionNumber;
+
+//! Project version string for NASAImagesCore.
+FOUNDATION_EXPORT const unsigned char NASAImagesCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NASAImagesCore/PublicHeader.h>
+
+
---
+++ NASAImagesUI/NASAImagesUI/NASAImagesUI.h
@@ -0,0 +1,19 @@
+//
+//  NASAImagesUI.h
+//  NASAImagesUI
+//
+//  Created by Robert Dresler on 07/05/2020.
+//  Copyright © 2020 Robert Dresler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NASAImagesUI.
+FOUNDATION_EXPORT double NASAImagesUIVersionNumber;
+
+//! Project version string for NASAImagesUI.
+FOUNDATION_EXPORT const unsigned char NASAImagesUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NASAImagesUI/PublicHeader.h>
+
+
