38d681bc4341ba2aa90dbef30e63bdf9fe31cbcd
Robert Dresler <robertdreslerjr@gmail.com>
2020-05-07 09:20:47 +0200

    Initial Commit

---
+++ NasaImages/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  NasaImages
+//
+//  Created by Robert Dresler on 07/05/2020.
+//  Copyright © 2020 Robert Dresler. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    private var rootController: UINavigationController {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController = UINavigationController()
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        return navigationController
+    }
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let _ = rootController
+        return true
+    }
+
+}
+
---
+++ NasaImages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NasaImages
+//
+//  Created by Robert Dresler on 07/05/2020.
+//  Copyright © 2020 Robert Dresler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
